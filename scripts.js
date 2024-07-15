dae1a1aa8cc2986e9e7630fdb5dbcbe30f051c50
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-07-15 06:45:06 -0300

    adicionado funcionalidade de enviar mensagem

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const url = 'https://mock-api.driven.com.br/api/v3/uol/'
+let mensagens;
+let usuario;
 
 function entrarNaSala() {
+    usuario = '';
     const name = document.querySelector('.nome').value;
     const entrar = axios.post(`${url}participants`, { name });
     entrar.then(esconderInicial)
@@ -10,6 +13,7 @@ function entrarNaSala() {
             erro.classList.remove('escondido')
         }
     })
+    usuario = name;
 
 }
 
@@ -18,4 +22,52 @@ function esconderInicial() {
     entrada.classList.add('escondido');
 }
 
+function lerMensagens() {
+    axios.get(`${url}messages`).then(response => {
+        mensagens = response.data;
+        renderizarMessages()
+    }).catch(error => {
+        console.error("Falha ao carregar mensagens:", error.response.status);
+    });
+}
 
+function renderizarMessages() {
+    const chat = document.querySelector('.messages')
+    chat.innerHTML = ''
+
+    mensagens.map(message => {
+        const { from, text, time, to, type } = message;
+        const elemento = `<li class="message ${type}">
+                <p>
+                    <span class="hora">(${time}) </span>
+                    <span class="remetente">${from}</span>
+                    <span class="visibilidade">para</span>
+                    <span class="destinatario">${to}:</span>
+                    <span class="texto">${text}</span>
+                </p>
+            </li>`
+        chat.innerHTML += elemento;
+
+        const objDiv = document.querySelector('.messages');
+        objDiv.scrollTop = objDiv.scrollHeight;
+    })
+}
+
+function enviarMensagem() {
+    const text = document.querySelector('.enviar-message input').value;
+    const msg = {
+        from: usuario,
+        to: "Todos",
+        text: text,
+        type: "message",
+    }
+    axios.post(`${url}messages`, msg)
+    console.log(msg)
+}
+
+function atualizarMensagem() {
+    setInterval(lerMensagens, 10000)
+}
+
+atualizarMensagem()
+lerMensagens();
